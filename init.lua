53ad4e920f88e4686826a1ca2edda8e0c6b9531b
Ellen Hui <ellenfkh@gmail.com>
2014-11-14 22:19:17 -0800

    move server stuff into init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 local application = require "mjolnir.application"
 local hotkey = require "mjolnir.hotkey"
 local window = require "mjolnir.window"
@@ -10,15 +9,15 @@ local itunes = require "mjolnir.lb.itunes"
 
 -- move window 10 spaces
 function move10 ()
-   local win = window.focusedwindow()
-      local f = win:frame()
-        f.x = f.x + 10
-          win:setframe(f)
-        end
+    local win = window.focusedwindow()
+    local f = win:frame()
+    f.x = f.x + 10
+    win:setframe(f)
+end
 
 -- change window frame
 function frameChange ()
-	local win = window.focusedwindow()
+    local win = window.focusedwindow()
     local frame = win:frame()
     local animation_time = 0.2
 
@@ -31,42 +30,50 @@ local wins = window.visiblewindows()
 
 -- change window focus
 function changeFocus()
-	head = table.remove(wins,1)
-	print(head:id())
-	sizey = #wins
-	print(sizey)
-	head:focus()
-	table.insert(wins,head)
+    head = table.remove(wins,1)
+    print(head:id())
+    sizey = #wins
+    print(sizey)
+    head:focus()
+    table.insert(wins,head)
 end
 
 -- minimize the window in focus
 function minWindow()
-	local win = window.focusedwindow()
-	if (win == nil) then 
-		return 
-	end
-	win:minimize()
+    local win = window.focusedwindow()
+    if (win == nil) then
+        return
+    end
+    win:minimize()
 end
 
 -- unminimize a window. if you care which window, TOO FUCKING BAD
 function unMinWindow()
-	for i, wn in pairs(window.allwindows()) do
-		if (wn:isminimized()) then
-			wn:unminimize()
-			break
-		end
-	end
-	
+    for i, wn in pairs(window.allwindows()) do
+        if (wn:isminimized()) then
+            wn:unminimize()
+            break
+        end
+    end
+
 end
 
 function launchChrome()
-	application.launchorfocus("Google Chrome")
+    application.launchorfocus("Google Chrome")
 end
 
 function launchiTunes()
-	application.launchorfocus("iTunes")
+    application.launchorfocus("iTunes")
 end
 
+function doCommand(comm)
+    i = math.floor(tonumber(comm))
+
+    if (i == 0) then print("got a zero. what do you want?!?!")
+    elseif (i == 1) then print ("got a one. what do you want?!?!?")
+    else print ("command not recognized")
+    end
+end
 
 
 
@@ -77,8 +84,19 @@ hotkey.bind({"ctrl"}, "m", unMinWindow)
 hotkey.bind({"ctrl"}, "c", launchChrome)
 hotkey.bind({"ctrl"}, "i", launchiTunes)
 
-
 hotkey.bind({"cmd", "alt", "ctrl"}, "D", frameChange)
 
+-- socket-y
+local socket = require("socket")
 
+local server = assert(socket.bind("*", 3456))
 
+while 1 do
+    local client = server:accept()
+    local command, err = client:receive("*l")
+
+    if not err then
+        num = tonumber(command)
+        print(command)
+    end
+end
